9d085366d394e8cbd952e4a6e3e1781a5dad26a3
Jose Luis <jj@jj>
2022-08-28 14:17:08 -0500

    login funcionando

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/register">Register</router-link>
+    <router-link to="/register">Registrarse</router-link> |
+    <router-link to="/login">Ingresar</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Login/>
+    </div>
+</template>
+
+<script>
+    import Login from '@/components/LoginComponent.vue';
+    export default {
+        name: 'LoginComponent',
+        components: {
+            Login
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
