b14665d98c045e1c1cbd7dcd9ba137912d4a1d78
Jose Luis <jj@jj>
2022-08-28 13:32:08 -0500

    registro funcionando

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="proyecto de pospercity en produccion"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/VentanaPrincipal.vue'
 
 export default {
   name: 'HomeView',
@@ -16,3 +16,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .home img {
+    width: 30%;
+    height: 30%;
+  }
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Register/>
+    </div>
+</template>
+
+<script>
+    import Register from '@/components/RegisterComponent.vue'
+    export default {
+        name: 'registerView',
+        components: {
+            Register
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
